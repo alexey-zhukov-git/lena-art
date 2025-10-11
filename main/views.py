df5fc209a5ccf9138211d8aa1b9fbf3dbc356ece
alexeyzhukov <myshakhovskaya@yandex.ru>
2025-10-12 00:31:05 +0300

    Работаем

--- main/views.py
+++ main/views.py
@@ -16,6 +16,7 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    quantity = Product.objects.filter(available=True).count()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
@@ -25,7 +26,8 @@ def product_list(request, category_slug=None):
         {
             'category': category,
             'categories': categories,
-            'products': products
+            'products': products,
+            'quantity': quantity
         }
     )
 
